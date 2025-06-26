3812b217652ddfc404c3b36a49de8248fefa5cfd
unknown <willian.hilton@hotmail.com>
2025-06-26 18:56:18 -0300

    Optimization in the event handler return

--- samples/WH.SimpleMediator.Examples/Events/ExampleEventHandler.cs
+++ samples/WH.SimpleMediator.Examples/Events/ExampleEventHandler.cs
@@ -1,12 +1,9 @@
-﻿using WH.SimpleMediator.Examples.ResourceManagers;
-
-namespace WH.SimpleMediator.Examples.Events;
+﻿namespace WH.SimpleMediator.Examples.Events;
 
 public sealed class ExampleEventHandler : INotificationHandler<ExampleEvent>
 {
     public Task Handle(ExampleEvent notification, CancellationToken cancellationToken)
     {
-        return Task.FromResult(
-            Result.CreateResponseWithData("Event proccessed successfully"));
+        return Task.CompletedTask;
     }
 }
