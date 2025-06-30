97f07e76949a4882c1a5f0230405f8f2a999b411
unknown <willian.hilton@hotmail.com>
2025-06-30 14:03:19 -0300

    Fix in handlers with dependency injection

--- src/WH.SimpleMediator.Core/Wrappers/NotificationHandlerWrapper.cs
+++ src/WH.SimpleMediator.Core/Wrappers/NotificationHandlerWrapper.cs
@@ -4,23 +4,24 @@ using System.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using System.Linq;
 
-namespace WH.SimpleMediator
+namespace WH.SimpleMediator;
+
+internal abstract class NotificationHandlerWrapperBase
 {
-    public abstract class NotificationHandlerWrapperBase
-    {
-        public abstract Task Handle(INotification notification, IServiceProvider serviceProvider,
-            CancellationToken cancellationToken);
-    }
+    public abstract Task Handle(INotification notification, IServiceProvider serviceProvider,
+        CancellationToken cancellationToken);
+}
 
-    public class NotificationHandlerWrapper<TNotification> : NotificationHandlerWrapperBase
-        where TNotification : INotification
+internal class NotificationHandlerWrapper<TNotification> : NotificationHandlerWrapperBase
+    where TNotification : INotification
+{
+    public async override Task Handle(INotification notification, IServiceProvider serviceProvider, CancellationToken cancellationToken)
     {
-        public async override Task Handle(INotification notification, IServiceProvider serviceProvider, CancellationToken cancellationToken)
-        {
-            var handlers = serviceProvider.GetServices<INotificationHandler<TNotification>>();
-            var tasks = handlers.Select(x => x.Handle((TNotification)notification, cancellationToken));
+        using var scope = serviceProvider.CreateScope();
 
-            await Task.WhenAll(tasks);
-        }
+        var handlers = scope.ServiceProvider.GetServices<INotificationHandler<TNotification>>();
+        var tasks = handlers.Select(x => x.Handle((TNotification)notification, cancellationToken));
+
+        await Task.WhenAll(tasks);
     }
 }
--- src/WH.SimpleMediator.Core/Wrappers/RequestHandlerWrapper.cs
+++ src/WH.SimpleMediator.Core/Wrappers/RequestHandlerWrapper.cs
@@ -4,29 +4,28 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace WH.SimpleMediator
+namespace WH.SimpleMediator;
+
+internal abstract class RequestHandlerWrapperBase<TResponse>
 {
-    public abstract class RequestHandlerWrapperBase<TResponse>
+    public abstract Task<TResponse> Handle(IRequest<TResponse> request, IServiceProvider serviceProvider,
+        CancellationToken cancellationToken);
+}
+
+internal class RequestHandlerWrapper<TRequest, TResponse> : RequestHandlerWrapperBase<TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public override Task<TResponse> Handle(IRequest<TResponse> request, IServiceProvider serviceProvider, CancellationToken cancellationToken)
     {
-        public abstract Task<TResponse> Handle(IRequest<TResponse> request, IServiceProvider serviceProvider,
-            CancellationToken cancellationToken);
-    }
+        using var scope = serviceProvider.CreateScope();
 
-    public class RequestHandlerWrapper<TRequest, TResponse> : RequestHandlerWrapperBase<TResponse>
-        where TRequest : IRequest<TResponse>
-    {
-        public override Task<TResponse> Handle(IRequest<TResponse> request, IServiceProvider serviceProvider, CancellationToken cancellationToken)
-        {
-            Task<TResponse> Handler(CancellationToken t = default) => serviceProvider.GetRequiredService<IRequestHandler<TRequest, TResponse>>()
-            .Handle((TRequest)request, t == default ? cancellationToken : t);
+        Task<TResponse> Handler(CancellationToken t = default) => scope.ServiceProvider.GetRequiredService<IRequestHandler<TRequest, TResponse>>()
+        .Handle((TRequest)request, t == default ? cancellationToken : t);
 
-            using var scope = serviceProvider.CreateScope();
-
-            return scope.ServiceProvider
-                .GetServices<IPipelineBehavior<TRequest, TResponse>>()
-                .Reverse()
-                .Aggregate((RequestHandlerDelegate<TResponse>)Handler,
-                    (next, pipeline) => (t) => pipeline.Handle((TRequest)request, next, t == default ? cancellationToken : t))();
-        }
+        return scope.ServiceProvider
+            .GetServices<IPipelineBehavior<TRequest, TResponse>>()
+            .Reverse()
+            .Aggregate((RequestHandlerDelegate<TResponse>)Handler,
+                (next, pipeline) => (t) => pipeline.Handle((TRequest)request, next, t == default ? cancellationToken : t))();
     }
 }
